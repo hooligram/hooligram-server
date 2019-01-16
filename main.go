82bab072f347ce42e3f746cb4052a5378d9df995
cglotr <amir.ariffin.920404@gmail.com>
2019-01-16 22:17:47 +0800

    echo action json

--- main.go
+++ main.go
@@ -1,9 +1,39 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
 )
 
+var upgrader = websocket.Upgrader{}
+
+type action struct {
+	Payload interface{} `json:"payload"`
+	Type    string      `json:"type"`
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	router := mux.NewRouter()
+	router.HandleFunc("/echo", echo)
+	http.ListenAndServe(":8080", router)
+}
+
+func echo(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		return
+	}
+
+	action := action{}
+
+	for {
+		err = conn.ReadJSON(&action)
+		if err != nil {
+			continue
+		}
+
+		conn.WriteJSON(action)
+	}
 }
