df377f23d331d7ac81a5030a85300e73740d660a
cglotr <amir.ariffin.920404@gmail.com>
2019-01-20 11:35:24 +0800

    add addone endpoint

--- main.go
+++ main.go
@@ -15,10 +15,38 @@ type Action struct {
 	Type    string                 `json:"type"`
 }
 
+// AddOneAction ...
+type AddOneAction struct {
+	Payload struct {
+		Count int `json:"count"`
+	} `json:"payload"`
+	Type string `json:"type"`
+}
+
 func main() {
 	router := mux.NewRouter()
+	router.HandleFunc("/addone", addOne)
 	router.HandleFunc("/echo", echo)
 	http.ListenAndServe(":8080", router)
+}
+
+func addOne(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		return
+	}
+
+	defer conn.Close()
+
+	for {
+		action := AddOneAction{}
+		err := conn.ReadJSON(&action)
+		if err != nil {
+			continue
+		}
+		action.Payload.Count++
+		conn.WriteJSON(action)
+	}
 }
 
 func echo(w http.ResponseWriter, r *http.Request) {
