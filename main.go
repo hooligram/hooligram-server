931e70d5368f777c350f60ec95defd5ec8f92011
cglotr <amir.ariffin.920404@gmail.com>
2019-01-28 22:59:19 +0800

    mysql db

--- main.go
+++ main.go
@@ -1,24 +1,51 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 )
 
 func main() {
 	port := os.Getenv("PORT")
-
 	if port == "" {
 		log.Fatal("PORT must be set. Exiting...")
 	}
 
 	twilioAPIKey = os.Getenv("TWILIO_API_KEY")
-
 	if twilioAPIKey == "" {
 		log.Fatal("TWILIO_API_KEY must be set. Exiting...")
+	}
+
+	mysqlUsername := os.Getenv("MYSQL_USERNAME")
+	if mysqlUsername == "" {
+		log.Fatal("MYSQL_USERNAME must be set. Exiting...")
+	}
+
+	mysqlPassword := os.Getenv("MYSQL_PASSWORD")
+	if mysqlPassword == "" {
+		log.Fatal("MYSQL_PASSWORD must be set. Exiting...")
+	}
+
+	mysqlDbName := os.Getenv("MYSQL_DB_NAME")
+	if mysqlDbName == "" {
+		log.Fatal("MYSQL_DB_NAME must be set. Exiting...")
+	}
+
+	db, err := sql.Open("mysql", mysqlUsername+":"+mysqlPassword+"@/"+mysqlDbName)
+	if err != nil {
+		log.Fatal("Error setting up MySQL DB connection. Exiting...")
+	}
+
+	db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("Can't connect to MySQL DB. Exiting...")
 	}
 
 	router := mux.NewRouter()
