2aa3a2019be5b175d0b49df50dd888594487338b
cglotr <amir.ariffin.920404@gmail.com>
2019-01-30 00:59:37 +0800

    refactor db

---
+++ db.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"os"
+)
+
+func init() {
+	log.Println("[DB] Initializing DB...")
+
+	dbUsername := os.Getenv("MYSQL_USERNAME")
+
+	if dbUsername == "" {
+		log.Fatal("[DB] MYSQL_USERNAME must be set. Exiting...")
+	}
+
+	dbPassword := os.Getenv("MYSQL_PASSWORD")
+
+	if dbPassword == "" {
+		log.Fatal("[DB] MYSQL_PASSWORD must be set. Exiting...")
+	}
+
+	dbName := os.Getenv("MYSQL_DB_NAME")
+
+	if dbName == "" {
+		log.Fatal("[DB] MYSQL_DB_NAME must be set. Exiting...")
+	}
+
+	var err error
+	db, err = sql.Open("mysql", dbUsername+":"+dbPassword+"@/"+dbName)
+
+	if err != nil {
+		log.Fatal("[DB] Error setting up MySQL DB connection. Exiting...")
+	}
+
+	err = db.Ping()
+
+	if err != nil {
+		log.Fatal("[DB] Can't connect to MySQL DB. Exiting...")
+	}
+
+	log.Println("[DB] Creating DB tables...")
+	db.Exec(`
+		CREATE TABLE IF NOT EXISTS client (
+			id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+			country_code VARCHAR (50) NOT NULL,
+			phone_number VARCHAR (50) NOT NULL,
+			verification_code VARCHAR (50),
+			PRIMARY KEY (id)
+		)
+	`)
+}
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"database/sql"
 	"log"
 	"net/http"
 	"os"
@@ -11,58 +10,25 @@ import (
 )
 
 func main() {
+	log.Println("[MAIN] Starting...")
+	defer db.Close()
+
 	port := os.Getenv("PORT")
+
 	if port == "" {
-		log.Fatal("PORT must be set. Exiting...")
+		log.Fatal("[MAIN] PORT must be set. Exiting...")
 	}
 
 	twilioAPIKey = os.Getenv("TWILIO_API_KEY")
+
 	if twilioAPIKey == "" {
-		log.Fatal("TWILIO_API_KEY must be set. Exiting...")
+		log.Fatal("[MAIN] TWILIO_API_KEY must be set. Exiting...")
 	}
-
-	mysqlUsername := os.Getenv("MYSQL_USERNAME")
-	if mysqlUsername == "" {
-		log.Fatal("MYSQL_USERNAME must be set. Exiting...")
-	}
-
-	mysqlPassword := os.Getenv("MYSQL_PASSWORD")
-	if mysqlPassword == "" {
-		log.Fatal("MYSQL_PASSWORD must be set. Exiting...")
-	}
-
-	mysqlDbName := os.Getenv("MYSQL_DB_NAME")
-	if mysqlDbName == "" {
-		log.Fatal("MYSQL_DB_NAME must be set. Exiting...")
-	}
-
-	var err error
-
-	db, err = sql.Open("mysql", mysqlUsername+":"+mysqlPassword+"@/"+mysqlDbName)
-	if err != nil {
-		log.Fatal("Error setting up MySQL DB connection. Exiting...")
-	}
-
-	defer db.Close()
-
-	err = db.Ping()
-	if err != nil {
-		log.Fatal("Can't connect to MySQL DB. Exiting...")
-	}
-
-	db.Exec(`
-		CREATE TABLE IF NOT EXISTS client (
-			id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-			country_code VARCHAR (50) NOT NULL,
-			phone_number VARCHAR (50) NOT NULL,
-			verification_code VARCHAR (50),
-			PRIMARY KEY (id)
-		)
-	`)
 
 	router := mux.NewRouter()
 	router.HandleFunc("/addone", addone)
 	router.HandleFunc("/echo", echo)
 	router.HandleFunc("/v1", v1)
+
 	http.ListenAndServe(":"+port, router)
 }
