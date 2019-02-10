842762ecd9b90c83070f6129e7c3aa4913a686d0
cglotr <amir.ariffin.920404@gmail.com>
2019-02-10 11:23:32 +0800

    refactor broadcast request

--- client.go
+++ client.go
@@ -4,6 +4,10 @@ func (client *Client) writeEmptyAction(actionType string) {
 	writeEmptyAction(client.conn, actionType)
 }
 
+func (client *Client) writeFailure(actionType string, errors []string) {
+	writeFailure(client.conn, actionType, errors)
+}
+
 func (client *Client) writeJSON(action *Action) {
 	client.conn.WriteJSON(*action)
 }
