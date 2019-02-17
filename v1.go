8c70660feed5e86712a0d97b561f567b893dff0a
cglotr <amir.ariffin.920404@gmail.com>
2019-02-17 15:24:38 +0800

    try to create client before making api call

--- v1.go
+++ v1.go
@@ -110,6 +110,22 @@ func handleVerificationRequestCodeRequest(conn *websocket.Conn, action *Action) 
 		return
 	}
 
+	client, err := getOrCreateClient(countryCode, phoneNumber)
+
+	if err != nil {
+		errors = append(errors, err.Error())
+		writeFailure(conn, verificationRequestCodeFailure, errors)
+		return
+	}
+
+	err = unverifyClient(client, conn)
+
+	if err != nil {
+		errors = append(errors, err.Error())
+		writeFailure(conn, verificationRequestCodeFailure, errors)
+		return
+	}
+
 	resp, err := postTwilioVerificationStart(countryCode, phoneNumber)
 
 	if err != nil {
@@ -138,22 +154,6 @@ func handleVerificationRequestCodeRequest(conn *websocket.Conn, action *Action) 
 
 	if !r["success"].(bool) {
 		errors = append(errors, "i failed to make verification start api call")
-		writeFailure(conn, verificationRequestCodeFailure, errors)
-		return
-	}
-
-	client, err := getOrCreateClient(countryCode, phoneNumber)
-
-	if err != nil {
-		errors = append(errors, err.Error())
-		writeFailure(conn, verificationRequestCodeFailure, errors)
-		return
-	}
-
-	err = unverifyClient(client, conn)
-
-	if err != nil {
-		errors = append(errors, err.Error())
 		writeFailure(conn, verificationRequestCodeFailure, errors)
 		return
 	}
