0b3dc1a43a5cfc2af5851c908f42405a20dbb103
cglotr <amir.ariffin.920404@gmail.com>
2019-02-19 22:44:01 +0800

    always put space before sql bracket

--- db.go
+++ db.go
@@ -44,9 +44,9 @@ func init() {
 	db.Exec(`
 		CREATE TABLE IF NOT EXISTS client (
 			id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-			country_code VARCHAR(50) NOT NULL,
-			phone_number VARCHAR(50) NOT NULL,
-			verification_code VARCHAR(50),
+			country_code VARCHAR (50) NOT NULL,
+			phone_number VARCHAR (50) NOT NULL,
+			verification_code VARCHAR (50),
 			PRIMARY KEY (id),
 			UNIQUE KEY (country_code, phone_number)
 		)
