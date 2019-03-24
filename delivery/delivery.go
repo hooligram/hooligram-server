6bb50ed20e29b1b779a155e31145e3d73e1ea1f3
cglotr <amir.ariffin.920404@gmail.com>
2019-03-24 15:05:04 +0800

    move to delivery package

--- delivery/delivery.go
+++ delivery/delivery.go
@@ -1,6 +1,11 @@
 package delivery
 
-import "github.com/hooligram/hooligram-server/db"
+import (
+	"github.com/hooligram/hooligram-server/actions"
+	"github.com/hooligram/hooligram-server/clients"
+	"github.com/hooligram/hooligram-server/db"
+	"github.com/hooligram/hooligram-server/utils"
+)
 
 // MessageDelivery .
 type MessageDelivery struct {
@@ -9,6 +14,24 @@ type MessageDelivery struct {
 }
 
 var messageDeliveryChan = make(chan *MessageDelivery)
+
+// DeliverMessage .
+func DeliverMessage() {
+	for {
+		messageDelivery := <-GetMessageDeliveryChan()
+		message := messageDelivery.Message
+		recipientIDs := messageDelivery.RecipientIDs
+
+		for _, client := range clients.GetSignedInClients() {
+			if !utils.ContainsID(recipientIDs, client.GetID()) {
+				continue
+			}
+
+			action := actions.CreateMessagingDeliverRequest(message)
+			client.WriteJSON(action)
+		}
+	}
+}
 
 // GetMessageDeliveryChan .
 func GetMessageDeliveryChan() chan *MessageDelivery {
--- main.go
+++ main.go
@@ -8,10 +8,7 @@ import (
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
-	"github.com/hooligram/hooligram-server/actions"
-	"github.com/hooligram/hooligram-server/clients"
 	"github.com/hooligram/hooligram-server/delivery"
-	"github.com/hooligram/hooligram-server/utils"
 	"github.com/hooligram/hooligram-server/v2"
 )
 
@@ -26,24 +23,7 @@ func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/v2", v2.V2)
 
-	go deliverMessage()
+	go delivery.DeliverMessage()
 
 	http.ListenAndServe(":"+port, router)
-}
-
-func deliverMessage() {
-	for {
-		messageDelivery := <-delivery.GetMessageDeliveryChan()
-		message := messageDelivery.Message
-		recipientIDs := messageDelivery.RecipientIDs
-
-		for _, client := range clients.GetSignedInClients() {
-			if !utils.ContainsID(recipientIDs, client.GetID()) {
-				continue
-			}
-
-			action := actions.CreateMessagingDeliverRequest(message)
-			client.WriteJSON(action)
-		}
-	}
 }
