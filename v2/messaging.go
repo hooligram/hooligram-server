6e6265167a2feeabc533dc5e65f28a946c49b867
cglotr <amir.ariffin.920404@gmail.com>
2019-03-26 16:30:45 +0800

    messaging action id

--- v2/messaging.go
+++ v2/messaging.go
@@ -8,56 +8,49 @@ import (
 	"github.com/hooligram/hooligram-server/utils"
 )
 
+/////////////////////////////////////
+// HANDLER: MESSAGING_SEND_REQUEST //
+/////////////////////////////////////
+
 func handleMessagingSendRequest(client *clients.Client, action *actions.Action) *actions.Action {
+	requestID := action.ID
+	if requestID == "" {
+		return messagingSendFailure(client, requestID, "id not in action")
+	}
+
 	content, ok := action.Payload["content"].(string)
 	if !ok {
-		failure := actions.MessagingSendFailure([]string{"content not in payload"})
-		client.WriteJSON(failure)
-		return failure
+		return messagingSendFailure(client, requestID, "content not in payload")
 	}
 
 	messageGroupID, ok := action.Payload["message_group_id"].(float64)
 	if !ok {
-		failure := actions.MessagingSendFailure([]string{"message_group_id not in payload"})
-		client.WriteJSON(failure)
-		return failure
+		return messagingSendFailure(client, requestID, "message_group_id not in payload")
 	}
 
 	senderID, ok := action.Payload["sender_id"].(float64)
 	if !ok {
-		failure := actions.MessagingSendFailure([]string{"sender_id not in payload"})
-		client.WriteJSON(failure)
-		return failure
+		return messagingSendFailure(client, requestID, "sender_id not in payload")
 	}
 
 	if client.GetID() != int(senderID) {
-		failure := actions.MessagingSendFailure([]string{"sender id mismatch"})
-		client.WriteJSON(failure)
-		return failure
+		return messagingSendFailure(client, requestID, "sender id mismatch")
 	}
 
 	if !db.ReadIsClientInMessageGroup(int(senderID), int(messageGroupID)) {
-		failure := actions.MessagingSendFailure(
-			[]string{"sender doesn't belong to message group"},
-		)
-		client.WriteJSON(failure)
-		return failure
+		return messagingSendFailure(client, requestID, "sender doesn't belong to message group")
 	}
 
 	message, err := db.CreateMessage(content, int(messageGroupID), int(senderID))
 	if err != nil {
 		utils.LogBody(v2Tag, "error creating message. "+err.Error())
-		failure := actions.MessagingSendFailure([]string{"server error"})
-		client.WriteJSON(failure)
-		return failure
+		return messagingSendFailure(client, requestID, "server error")
 	}
 
 	messageGroupMemberIDs, err := db.ReadMessageGroupMemberIDs(message.MessageGroupID)
 	if err != nil {
 		utils.LogBody(v2Tag, "error finding meesage group member ids. "+err.Error())
-		failure := actions.MessagingSendFailure([]string{"server error"})
-		client.WriteJSON(failure)
-		return failure
+		return messagingSendFailure(client, requestID, "server error")
 	}
 
 	var recipientIDs = make([]int, len(messageGroupMemberIDs))
@@ -80,34 +73,57 @@ func handleMessagingSendRequest(client *clients.Client, action *actions.Action) 
 	}
 
 	success := actions.MessagingSendSuccess(message.ID)
+	success.ID = requestID
 	client.WriteJSON(success)
 	return success
 }
 
+////////////////////////////////////////
+// HANDLER: MESSAGING_DELIVER_SUCCESS //
+////////////////////////////////////////
+
 func handleMessagingDeliverSuccess(client *clients.Client, action *actions.Action) *actions.Action {
+	requestID := action.ID
+	if requestID == "" {
+		return messagingDeliverSuccessFailure(client, requestID, "id not in action")
+	}
+
 	messageID, ok := action.Payload["message_id"].(float64)
 	if !ok {
-		failure := actions.MessagingDeliverSuccessFailure([]string{"message_id not in payload"})
-		client.WriteJSON(failure)
-		return failure
+		return messagingDeliverSuccessFailure(client, requestID, "message_id not in payload")
 	}
 
 	recipientID := client.GetID()
 	ok, err := db.UpdateReceiptDateDelivered(int(messageID), recipientID)
 	if err != nil {
 		utils.LogBody(v2Tag, "error updating receipt date delivered. "+err.Error())
-		failure := actions.MessagingDeliverSuccessFailure([]string{"server error"})
-		client.WriteJSON(failure)
-		return failure
+		return messagingDeliverSuccessFailure(client, requestID, "server error")
 	}
 
 	if !ok {
-		failure := actions.MessagingDeliverSuccessFailure([]string{"not allowed"})
-		client.WriteJSON(failure)
-		return failure
+		return messagingDeliverSuccessFailure(client, requestID, "not allowed")
 	}
 
 	success := actions.MessagingDeliverSuccessSuccess(int(messageID))
+	success.ID = requestID
 	client.WriteJSON(success)
 	return success
+}
+
+////////////
+// HELPER //
+////////////
+
+func messagingSendFailure(client *clients.Client, actionID, err string) *actions.Action {
+	failure := actions.MessagingSendFailure([]string{err})
+	failure.ID = actionID
+	client.WriteJSON(failure)
+	return failure
+}
+
+func messagingDeliverSuccessFailure(client *clients.Client, actionID, err string) *actions.Action {
+	failure := actions.MessagingDeliverSuccessFailure([]string{err})
+	failure.ID = actionID
+	client.WriteJSON(failure)
+	return failure
 }
